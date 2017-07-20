1f7ba32442099d2df802a4b602f366a08c2552f2
Marc Vergara <marc.vergara.f@gmail.com>
2017-07-20 20:35:37 +0200

    working show albums of artits

--- js/controller/listController.js
+++ js/controller/listController.js
@@ -1,10 +1,15 @@
 angular.module('spotyApp')
 .controller('listController', function ($scope, spotifyService) {
-  $scope.$on('usernameReady', function (e, data) {
-    spotifyService.getUserData(data.username)
+	$scope.$on('usernameReady', function (e, data) {
+		spotifyService.getUserData(data.username)
 		.then(function (oResponse) {
-  console.log(oResponse.data.artists.items[0].name)
-  // $scope.artistList = oResponse.data.artist.name
+			console.log(oResponse.data.artists.items[0].name)
+			$scope.results = true
+			$scope.artitsResult = oResponse.data.artists.items
+  // $scope.artistList = oResponse.data.artists.items[0].name
+  // $scope.artistImg = oResponse.data.artists.items[0].images[1].url
+  // return ($scope.results = !$scope.results)
+
 })
-  })
-})
+	})
+}) 
---
+++ js/controller/token.js
@@ -0,0 +1,14 @@
+// angular.module('spotyApp')
+// .controller('token', function($scope, $rootScope){
+// 	$scope.showToken = function () {
+// 		var tokenx = 'BQDZIxYviu2NQj0wWfUkyWtqUhlauVWSfgscAJXBsg9jDlRxSKkd0A6UQmSwb64ZVlLdweBGwTr1QZahqsnKteJiMXlNh5ppzBG6sezkqJv1QC_tCg1pPTAUItFxYnQaLhxfca48SCs'
+// 		var token = $scope.tokenx
+// 		$rootScope.$broadcast('token', { token: token })
+// 	}
+// })
+
+// $scope.showResult = function () {
+//     var username = $scope.nameArtist
+//     $rootScope.$broadcast('usernameReady', { username: username })
+//   }
+// })
